4712b36afa1f140e7068088464d4cbe30d48f9b7
mskasem <mskasem@ncsu.edu>
2024-06-27 17:23:27 -0400

    [Design] Updated the design with CloseCoding branding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Container } from "@/components/Container";
+import heroImg from "../../public/img/hero.jpg";
 
 export const Hero = () => {
   return (
@@ -8,12 +9,13 @@ export const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Free Landing Page Template for startups
+              Close the gap with custom solutions
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Nextly is a free landing page & marketing website
-              template for startups and indie projects. Its built with
-              Next.js & TailwindCSS. And its completely open-source.
+              At Close Coding, we specialize in transforming your
+              ideas into digital reality. Whether you need a stunning website,
+              powerful automation, bespoke software, or a cutting-edge mobile app,
+               our expert team is here to deliver custom solutions that perfectly fit your needs.
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
@@ -21,7 +23,7 @@ export const Hero = () => {
                 href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
                 target="_blank"
                 rel="noopener"
-                className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
+                className="px-8 py-4 text-lg font-medium text-center text-white bg-blue-600 rounded-md ">
                 Download for Free
               </a>
               <a
@@ -47,7 +49,7 @@ export const Hero = () => {
         </div>
         <div className="flex items-center justify-center w-full lg:w-1/2">
           <div className="">
-            {/* <Image
+            <Image
               src={heroImg}
               width="616"
               height="617"
@@ -55,14 +57,14 @@ export const Hero = () => {
               alt="Hero Illustration"
               loading="eager"
               placeholder="blur"
-            /> */}
+            />
           </div>
         </div>
       </Container>
       <Container>
         <div className="flex flex-col justify-center">
           <div className="text-xl text-center text-gray-700 dark:text-white">
-            Trusted by <span className="text-indigo-600">2000+</span>{" "}
+            Trusted by <span className="text-blue-600">2000+</span>{" "}
             customers worldwide
           </div>
 
